c3ad7448052e17ffbc313ad220c80494672c181d
Hadi <hadi_sabbagh02@outlook.com>
2024-05-01 21:46:49 +0300

    Removed unused file

--- src/main/java/com/sau/project2/ImageUtils/ImageStorageType.java
+++
@@ -1,6 +0,0 @@
-package com.sau.project2.ImageUtils;
-
-public class ImageStorageType {
-    public static final String S3STORAGE = "s3Storage";
-    public static final String LOCALSTORAGE = "localStorage";
-}
